0f6e579377b2254b783003780eac94cd47789621
Digitelektro <32802255+Digitelektro@users.noreply.github.com>
2018-05-06 12:44:27 +0200

    Remove local function declarations

--- PIC/I2C.h
+++ PIC/I2C.h
@@ -1,12 +1,7 @@
 //I2C.h
 
 
-void I2CWait ();
-void initI2C();
-void I2CStart(unsigned char Address);
-void I2CReStart(unsigned char Address);
-void I2CSend(unsigned char Data);
-void I2CStop (void);
+void I2CInit();
 
 void I2CRead(unsigned char Address, unsigned char Register, void *Data, unsigned int len);
 void I2CWriteBlock(unsigned char Address, unsigned char Register, void *Data, unsigned int len);
