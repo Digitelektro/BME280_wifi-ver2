790c53ab247a572527cc79dcb4a63925537c3bde
Digitelektro <32802255+Digitelektro@users.noreply.github.com>
2018-05-06 13:24:53 +0200

    Use #pragma config when XC16 compiler is used

--- PIC/main.c
+++ PIC/main.c
@@ -15,16 +15,49 @@
 #define FCY 59881250LL			//Define clock for delay_ms library
 #include <libpic30.h>			//This is need to use delay_ms function, #define FCY must be under #include <libpic.h> otherwise it's not working
 
-_FPOR(ALTI2C1_ON)				//I2C alternativ pin configuration  :RB8, RB9  
-_FGS(GWRP_OFF & GCP_OFF)		//Write protect and code protect disabled
-_FICD(ICS_PGD1 & JTAGEN_OFF)	//Debug pin select, disable JTAG
-_FWDT(FWDTEN_ON & WDTPOST_PS1024 & WDTPRE_PR32 & WINDIS_OFF)		//Enable watchdog, timeout will be around 1,024S
-_FOSCSEL(FNOSC_FRC & IESO_OFF);										// Select Internal FRC at POR
-_FOSC(FCKSM_CSECMD & OSCIOFNC_OFF  & POSCMD_NONE);  				// Clock switching is enabled,Fail-safe Clock Monitor is disabled
-																	// OSC2 Pin Function: OSC2 Clock Output disabled
-																	// Primary Oscillator Mode: NONE
-								
+#if defined(__XC16__)
+	// FICD
+	#pragma config ICS = PGD1               // ICD Communication Channel Select bits (Communicate on PGEC1 and PGED1)
+	#pragma config JTAGEN = OFF             // JTAG Enable bit (JTAG is disabled)
+	
+	// FPOR
+	#pragma config ALTI2C1 = ON             // Alternate I2C1 pins (I2C1 mapped to ASDA1/ASCL1 pins)
+	#pragma config ALTI2C2 = ON             // Alternate I2C2 pins (I2C2 mapped to ASDA2/ASCL2 pins)
+	#pragma config WDTWIN = WIN25           // Watchdog Window Select bits (WDT Window is 25% of WDT period)
+	
+	// FWDT
+	#pragma config WDTPOST = PS1024         // Watchdog Timer Postscaler bits (1:1,024)
+	#pragma config WDTPRE = PR32            // Watchdog Timer Prescaler bit (1:32)
+	#pragma config PLLKEN = ON              // PLL Lock Enable bit (Clock switch to PLL source will wait until the PLL lock signal is valid.)
+	#pragma config WINDIS = OFF             // Watchdog Timer Window Enable bit (Watchdog Timer in Non-Window mode)
+	#pragma config FWDTEN = ON              // Watchdog Timer Enable bit (Watchdog timer always enabled)
+	
+	// FOSC
+	#pragma config POSCMD = NONE            // Primary Oscillator Mode Select bits (Primary Oscillator disabled)
+	#pragma config OSCIOFNC = OFF           // OSC2 Pin Function bit (OSC2 is clock output)
+	#pragma config IOL1WAY = ON             // Peripheral pin select configuration (Allow only one reconfiguration)
+	#pragma config FCKSM = CSECMD           // Clock Switching Mode bits (Clock switching is enabled,Fail-safe Clock Monitor is disabled)
+	
+	// FOSCSEL
+	#pragma config FNOSC = FRCPLL           // Oscillator Source Selection (Fast RC Oscillator with divide-by-N with PLL module (FRCPLL))
+	#pragma config PWMLOCK = OFF            // PWM Lock Enable bit (PWM registers may be written without key sequence)
+	#pragma config IESO = OFF               // Two-speed Oscillator Start-up Enable bit (Start up with user-selected oscillator source)
+	
+	// FGS
+	#pragma config GWRP = OFF               // General Segment Write-Protect bit (General Segment may be written)
+	#pragma config GCP = OFF                // General Segment Code-Protect bit (General Segment Code protect is Disabled)
 
+#else						
+	
+	_FPOR(ALTI2C1_ON)				//I2C alternativ pin configuration  :RB8, RB9  
+	_FGS(GWRP_OFF & GCP_OFF)		//Write protect and code protect disabled
+	_FICD(ICS_PGD1 & JTAGEN_OFF)	//Debug pin select, disable JTAG
+	_FWDT(FWDTEN_ON & WDTPOST_PS1024 & WDTPRE_PR32 & WINDIS_OFF)		//Enable watchdog, timeout will be around 1,024S
+	_FOSCSEL(FNOSC_FRC & IESO_OFF);										// Select Internal FRC at POR
+	_FOSC(FCKSM_CSECMD & OSCIOFNC_OFF  & POSCMD_NONE);  				// Clock switching is enabled,Fail-safe Clock Monitor is disabled
+																		// OSC2 Pin Function: OSC2 Clock Output disabled
+																		// Primary Oscillator Mode: NONE
+#endif
 
 //************************Function  Declarations*********************************//
 void SleepSec(unsigned int Sec);
